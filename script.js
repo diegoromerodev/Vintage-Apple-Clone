322a359af5fd7cd20d621c4a2371af120370a6ee
Diego Romero <diegoromeropersonal@gmail.com>
2021-08-17 14:46:01 -0300

    Add search bar animation

---
+++ script.js
@@ -0,0 +1,14 @@
+const searchBar = document.querySelector("#search-bar input")
+const magnifyingGlass = document.querySelector("#search-bar img")
+
+searchBar.addEventListener("focus", () => {
+    searchBar.parentElement.style.flex = 1.7;
+    searchBar.parentElement.style.background = "rgba(240,240,240,1)";
+    magnifyingGlass.style.filter = "invert(20%)";
+})
+
+searchBar.addEventListener("blur", () => {
+    searchBar.parentElement.style.flex = 1;
+    searchBar.parentElement.style.background = "rgba(240,240,240,0.4)";
+    magnifyingGlass.style.filter = "invert(100%)";
+})
